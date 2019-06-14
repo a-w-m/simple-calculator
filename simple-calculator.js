2fe9fac3cc4ed55352bdcbd8835495d129c1b1df
a-w-m <47095419+a-w-m@users.noreply.github.com>
2019-06-14 17:25:13 -0400

    working calculator
    calculator can perform simple operations.
    
    Need to fix eval() limitations and rounding

--- simple-calculator.js
+++ simple-calculator.js
@@ -1,86 +1,144 @@
+(function calculator() {
+    let buttonInputArray = []
+    let isThereADecimal = false
 
-    function calculator() {
-        let calcArr = []
-        let calcStr = calcArr.join("")
+    const displayToTerminal = function() {
+        document.getElementById("terminal").innerHTML = buttonInputArray.join("")
+    }
+
+    const numbers = document.getElementsByClassName("numbers")
+
+    for (let x = 0; x < numbers.length; x++) {
+        numbers[x].onclick = function() {
+            if (document.getElementById("terminal").innerHTML.indexOf("=") != -1) {
+                buttonInputArray = []
+                buttonInputArray.push(numbers[x].innerHTML)
+                displayToTerminal();
+            } else {
+                buttonInputArray.push(numbers[x].innerHTML)
+                displayToTerminal();
+            }
+        }
+    }
+
+    const operations = document.getElementsByClassName("operations")
+
+    for (let operation = 0; operation < operations.length; operation++) {
+        operations[operation].onclick = function() {
+            if (buttonInputArray[buttonInputArray.length - 1] == "*" || buttonInputArray[buttonInputArray.length - 1] == "/" || buttonInputArray[buttonInputArray.length - 1] == "+" || buttonInputArray[buttonInputArray.length - 1] == "-" || buttonInputArray[buttonInputArray.length - 1] == ".") {
+
+                buttonInputArray.pop()
+                buttonInputArray.push(operations[operation].innerHTML)
+                displayToTerminal();
+                isThereADecimal = false;
+            } else {
+                buttonInputArray.push(operations[operation].innerHTML)
+                displayToTerminal();
+                isThereADecimal = false;
+            }
+        }
+
+    }
+
+    const equals = function () {
+
+        let myRegex = /[\*\+-\/]+$/
+
+        if (buttonInputArray[0] == '/' || buttonInputArray[0] == "*") {
+            buttonInputArray.unshift(0)
+            document.getElementById("terminal").innerHTML = buttonInputArray.join("") + "=" + eval(buttonInputArray.join(""));
+            buttonInputArray = [eval(buttonInputArray.join(""))]
+
+        } else if (myRegex.test(buttonInputArray.join(""))) {
+            let buttonInputString = buttonInputArray.join("").replace(myRegex, "")
+            document.getElementById("terminal").innerHTML = buttonInputString + "=" + eval(buttonInputString)
+            buttonInputArray = [eval(buttonInputString)]
+        } else {
+            document.getElementById("terminal").innerHTML = buttonInputArray.join("") + "=" + eval(buttonInputArray.join(""));
+            buttonInputArray = [eval(buttonInputArray.join(""))]
+        }
+    }
 
 
+    document.getElementById("equals").onclick = equals;
 
+    document.getElementById("allClear").onclick = function() {
+        buttonInputArray = [];
+        document.getElementById("terminal").innerHTML = "0"
+        isThereADecimal = false;
+    }
 
+    const deleteLastInput = function () {
+        if (buttonInputArray[buttonInputArray.length - 1] == ".") {
+            buttonInputArray.pop()
+            displayToTerminal();
+            isThereADecimal = false;
+        } else {
+            buttonInputArray.pop()
+            displayToTerminal();
+        }
+    }
 
-        function interface(){
+    document.getElementById("delete").onclick = deleteLastInput;
 
-            
-            document.getElementById("terminal").innerHTML = eval(calcStr)
-              
-            
-            document.getElementById("equals").onclick = function (){
-                
-                document.getElementById("terminal").innerHTML = eval(calcStr)
-            }
+ const decimal = function () {
+        if (isThereADecimal == false) {
+            buttonInputArray.push(".")
+            displayToTerminal();
+            isThereADecimal = true
 
-            document.getElementById("allClear").onclick = function (){
-                calcArr = [];
-            }
+        } else {
+            displayToTerminal();
+        }
 
-            document.getElementById("delete").onclick = function () {
-                calcArr.pop()
-            }
+    }
+    document.getElementById("decimal").onclick = decimal
 
-            document.getElementById("divide").onclick = function (){
-                calcArr.push('/')
+    document.addEventListener('keydown', (event) =>{
+
+        const keyPress = event.key
+        const operators = ["/", "*", "+", "-"]
+
+        if(!(isNaN(keyPress))){
+
+            if (document.getElementById("terminal").innerHTML.indexOf("=") != -1) {
+                buttonInputArray = []
+                buttonInputArray.push(keyPress)
+                displayToTerminal();
+            } else {
+                buttonInputArray.push(keyPress)
+                displayToTerminal();
             }
-            document.getElementById("multiply").onclick = function (){
-                calcArr.push('*')
+        }
+
+        else if(operators.indexOf(keyPress) != -1 ){
+
+            if (buttonInputArray[buttonInputArray.length - 1] == "*" || buttonInputArray[buttonInputArray.length - 1] == "/" || buttonInputArray[buttonInputArray.length - 1] == "+" || buttonInputArray[buttonInputArray.length - 1] == "-" || buttonInputArray[buttonInputArray.length - 1] == ".") {
+
+                buttonInputArray.pop()
+                buttonInputArray.push(keyPress)
+                displayToTerminal();
+                isThereADecimal = false;
+            } else {
+                buttonInputArray.push(keyPress)
+                displayToTerminal();
+                isThereADecimal = false;
             }
-            document.getElementById("subtract").onclick = function (){
-                calcArr.push('-')
-            }
-            document.getElementById("add").onclick = function(){
-                calcArr.push('+')
-            }
-            document.getElementById("seven").onclick = function (){
-                calcArr.push('7')
-            }
-            document.getElementById("eight").onclick = function(){
-                calcArr.push('8')
-            }
-            document.getElementById("nine").onclick = function (){
-                calcArr.push('9')
-            }
-           
-           document.getElementById("five").onclick = function (){
-                calcArr.push('5')
-            }
-            document.getElementById("six").onclick = function (){
-                calcArr.push('6')
-            }
-            document.getElementById("one").onclick = function (){
-                calcArr.push('1')
-            }
-            document.getElementById("two").onclick = function (){
-                calcArr.push('2')
-            }
-            document.getElementById("three").onclick = function (){
-                calcArr.push('3')
-            }
-            document.getElementById("decimal").onclick = function (){
-                calcArr.push('.')
-            }
-            document.getElementById("zero").onclick = function (){
-                calcArr.push('0')
-            }
-    
 
         }
-    
-                
+        else if(keyPress == "=" || keyPress.toLowerCase() == "enter"){
+            equals();
 
-            interface();
-    console.log(calcArr, calcStr)
+        }
+        else if(keyPress.toLowerCase() == "backspace"|| keyPress.toLowerCase() == "delete"){
+            deleteLastInput()
+        }
+        else if(keyPress == "."){
+            decimal();
+        }
 
 
+    })
 
 
-    
-    }
-    calculator();
+})();
